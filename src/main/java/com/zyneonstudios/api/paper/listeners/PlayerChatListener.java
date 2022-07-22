becea0dfb29f922e654b3fcaa1b9d24f8e684667
nerotvlive <daniel.niesmann@icloud.com>
2022-07-22 15:21:03 +0200

    1.6.0-SNAPSHOT (added ZyneonChatEvent.java)

---
+++ src/main/java/com/zyneonstudios/api/paper/listeners/PlayerChatListener.java
@@ -0,0 +1,21 @@
+package com.zyneonstudios.api.paper.listeners;
+
+import com.zyneonstudios.api.paper.events.ZyneonChatEvent;
+import org.bukkit.Bukkit;
+import org.bukkit.event.EventHandler;
+import org.bukkit.event.EventPriority;
+import org.bukkit.event.Listener;
+import org.bukkit.event.player.AsyncPlayerChatEvent;
+
+public class PlayerChatListener implements Listener {
+
+    @EventHandler(priority = EventPriority.HIGHEST)
+    public void onChat(AsyncPlayerChatEvent e) {
+        e.setCancelled(true);
+        ZyneonChatEvent event = new ZyneonChatEvent(e.getPlayer(),e.getMessage());
+        Bukkit.getPluginManager().callEvent(event);
+        if(!event.isCancelled()) {
+            Bukkit.broadcastMessage(event.getFormat());
+        }
+    }
+}
